f27d5471f661f95ea2945839096b7f31c846ccd5
sikudamobile <ya.sikuda@yandex.ru>
2026-07-30 13:02:15 +0300

    set init 2-1

--- pyside6/bigxmlwidget.py
+++ pyside6/bigxmlwidget.py
@@ -11,7 +11,7 @@ class XmlItemType(Enum):
     Attribute = 2
     Comment = 3
 
-intTreeInitialReadLevel   = 1 #initial open level
+intTreeInitialReadLevel   = 2 #initial open level
 intTreeInitialExpandLevel = 1 #initial expand level
 
 class BigXmlWidget(QTreeWidget, QWidget):
